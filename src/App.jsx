c36b7b46a2ca322eb689b86f6f0b690ca7ab1967
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-06-01 23:34:10 +0530

    initiate Youtube clone project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+import NavBar from "./layouts/NavBar";
+import Body from "./layouts/body/Body";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Body/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/layouts/body/MainContainer.jsx
@@ -0,0 +1,14 @@
+import ButtonList from "@/components/ButtonList";
+import VideoContainer from "@/components/VideoContainer";
+import React from "react";
+
+const MainContainer = () => {
+  return (
+    <div>
+      <ButtonList />
+      <VideoContainer />
+    </div>
+  );
+};
+
+export default MainContainer;
